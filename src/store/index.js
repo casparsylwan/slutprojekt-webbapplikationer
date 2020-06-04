23929db127d59a4c8a0b8ba1e5df2f722b5bac8f
casparsylwan <casparsylwan@gmail.com>
2020-06-04 13:59:20 +0200

    axiom set cors problem

--- src/store/index.js
+++ src/store/index.js
@@ -22,17 +22,20 @@ export default new Vuex.Store({
     registerNewCustomer(state, customer){
       state.customer.name = customer.name;
       console.log(customer);
+      console.log(customer.message);
+      console.log(1);
     }
   },
   actions: {
     async newCustomer({commit}, customer){
 
-      console.log(customer);
-      const resp = await axios.get('localhost:5000/');
-     // const response = await axios.post('localhost:5000/api/register/', {customer});
-     // commit('registerNewCustomer', response.data)
-     console.log(commit);
-      console.log(resp.data)
+     // console.log(customer +1);
+     // const response = await axios.get('http://localhost:5000/');
+      const response = await axios.post('http://localhost:5000/api/register', {...customer});
+    //  console.log(response.data)
+      commit('registerNewCustomer', response.data)
+    // console.log(commit);
+     // console.log(resp.data)
     }
   },
   modules: {
