5dc7ced146a0730e04d921becafc0e0423019bda
EHeijer <54702667+EHeijer@users.noreply.github.com>
2020-06-05 15:17:39 +0200

    Almost done with the style of the cart

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,13 +1,24 @@
 <template>
     <div class="cartItem">
-        <div class="title-desc-price">
-            <h3>{{title}}</h3>
-            <p>{{shortDesc}}</p>
-            <p>{{price}} KR</p>
-        </div>
         <div class="image">
-           <img :src="require('@/assets/' + imgFile)" alt="">
+            <img :src="require('@/assets/' + imgFile)" alt="">
         </div>
+        <div class="title-desc">
+            <h3>{{title}}</h3>
+            <p class="desc">{{shortDesc}}</p>
+        </div>
+        <div class="amount">
+            <img src="../assets/minus-solid.svg" alt="">
+            <input type="text" value="0">
+            <img src="../assets/plus-solid.svg" alt="">
+        </div>
+        <div class="price">
+            <p>{{price}} KR</p>
+        </div> 
+        <div class="remove">
+            <img src="../assets/remove.png" alt="">
+        </div> 
+        <hr>  
     </div>
 </template>
 <script>
@@ -31,30 +42,59 @@ export default {
 <style lang="scss" scoped>
     .cartItem {
         margin-bottom: 1rem;
-        .title-desc-price {
-            display: grid;
-            grid-template-columns: 50% 50%;
-            grid-template-rows: 50% 50%;
-            max-width: 190px;
-            margin: auto;
-            text-align: left;
-            
+        display: grid;
+        grid-template-columns:  10% 20% 30% 20% 10%;
+        align-items: center;
+        .title-desc {
+           align-self: center;
+           grid-column: 2 / span 1;
+           color: black;
             h3{
-                grid-column: 1 / span 1;
-                grid-row: 1 /   span 1;
-                margin: 0;
-                
-            }
-            p{
-                grid-row: 2 / span 1;
                 margin: 0;
             }
+
+            .desc {
+                margin: 0;
+            }
+            
         }
         .image {
             img {
-                width:150px;
+                width:120px;
             }
-            margin-bottom: 3rem;
         }
+
+        .amount {
+
+            img {
+                width: 12px;
+                height: 12px;
+                cursor: pointer;
+                
+            }
+
+            input {
+                width: 1rem;
+                padding: 6px 8px;
+                border-radius: 6px;
+                text-align: center;
+                border: transparent;
+                margin: 0 1rem;
+            }
+        }
+
+        .remove {
+            cursor: pointer;
+        }
+
+        .price {
+            color: black;
+        }
+
+        hr {
+            grid-column: 1 / span 5;
+            width: 100%;
+            border: solid rgba(134, 131, 131, 0.308) 1px;
+        }       
     }
 </style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -11,18 +11,27 @@
             :imgFile="product.imgFile"/>
         </article>
         <article class="order">
-
+            <h1>Order Details</h1>
+            <div class="shipping">
+                <label>Shipping</label>
+                <select id="select">
+                    <option value="Posten">Posten(49kr)</option>
+                    <option value="DB Schenker">DB Schenker(99kr)</option>
+                    <option value="DHL">DHL(119kr)</option>
+                </select>
+            </div>
+            <hr>
+            <h3>Total Cost</h3>
+            <h2>2500.00 KR</h2>
+            <button>Checkout</button>
         </article>
-        <Footer />
     </section>
 </template>
 <script>
-import Footer from "../components/Footer"
 import CartItem from "../components/CartItem"
 import { mapState } from "vuex";
 export default {
     components: {
-        Footer,
         CartItem
     },
     data: () => ({
@@ -54,20 +63,93 @@ export default {
         .cart {
             background: $shop-color;
             display: grid;
-            grid-template-columns: 50% 50%;
+            grid-template-columns: 9% 90% 1%;
 
             h1 {
                 font-size: 2.5rem;
+                font-weight: 300;
                 color: black;
+                grid-column: 2 / span 1;
+                justify-self: left;
             }
 
             .item {
-                grid-column: 1 / span 1;
+                grid-column: 2 / span 1;
+                
             }
         }
 
         .order {
             background: rgb(245, 241, 241);
+            
+            .shipping {
+                display: flex;
+                flex-direction: column;
+                justify-content: flex-end;
+                width: 70%;
+                margin: auto;
+                height: 50px;
+                padding: 2rem 1rem;
+                background:white;
+                border-radius: 8px;
+                text-align: left;
+                
+                label {
+                    margin-bottom: .5rem;
+                }
+                #select {
+                    
+                    border: none;
+                    font-size: 1.5rem;
+                    -webkit-appearance: none;
+                    -moz-appearance: none;
+                    appearance: none;
+                    background-image: url(../assets/arrow.png);
+                    background-repeat: no-repeat;
+                    background-position: 98% 95%;
+                    option {
+                        
+                    }
+                }
+
+                
+            }
+
+            hr {
+                width: 100%;
+                border: solid rgba(134, 131, 131, 0.158) 1px;
+                margin-top: 2rem;
+            }
+
+            h1 {
+                font-size: 2.5rem;
+                font-weight: 300;
+                color: black;
+                margin-bottom: 6rem;
+            }
+
+            h3 {
+                margin-top: 3rem;
+                margin-bottom: 0;
+                font-weight: 300;
+                font-size: 1.5rem;
+            }
+
+            button {
+                margin-top: 3rem;
+                width: 60%;
+                padding: 1rem;
+                border-radius: 10px;
+                border-style: none;
+                background: black;
+                color: white;
+                font-weight: bold;
+                font-size: 1.1rem;
+            }
+
+            h2 {
+                margin-top: 2px;
+            }
         }
     }
 </style>
